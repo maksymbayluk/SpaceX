0a66799b38ae5c2b9af5140a71f5ae9676ad73c4
Maksym Bailiuk <bailiukmaxym@gmail.com>
2025-05-22 19:02:16 +0200

    Initial Commit

---
+++ SpaceX/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SpaceX
+//
+//  Created by Максим Байлюк on 22.05.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
